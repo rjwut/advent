ebe06721bdc2a17c72b08cc3dcb5a763ef68ac3b
Robert J. Walker <rjw801@gmail.com>
2025-12-01 10:58:13 -0700

    Minor refactor

--- src/solutions/2016/day-05.js
+++ src/solutions/2016/day-05.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const { createHash } = require('crypto');
 
 const PASSWORD_LENGTH = 8;
 const HASH_PREFIX = '00000';
@@ -7,14 +7,13 @@ const ASCII_0 = 48;
 /**
  * # [Advent of Code 2016 Day 5](https://adventofcode.com/2016/day/5)
  *
- * Both parts are solved in a single pass, which is good because it takes a
- * while. (After I solved it, I hit up
+ * Both parts are solved in a single pass, which is good because it takes a while. (After I solved
+ * it, I hit up
  * [Reddit](https://www.reddit.com/r/adventofcode/comments/5gk2yv/2016_day_5_solutions/)
- * to see if there was some way that I could solve this without testing every
- * index along the way. The answer appears to be "no.") Each hash that it finds
- * starts with `'00000'` is checked for both passwords. The password for part
- * one will never be found before the one for part two, so it continues looping
- * until the part two password is complete.
+ * to see if there was some way that I could solve this without testing every index along the way.
+ * The answer appears to be "no.") Each hash that it finds starts with `'00000'` is checked for both
+ * passwords. The password for part one will never be found before the one for part two, so it
+ * continues looping until the part two password is complete.
  *
  * @param {string} input - the puzzle input
  * @returns {Array} - the puzzle answers
@@ -22,16 +21,16 @@ const ASCII_0 = 48;
 module.exports = input => {
   input = input.trim();
   let password1 = '', password2 = new Array(PASSWORD_LENGTH);
-  let i = 0, password2Length = 0;
+  let i = 0, password2CharsFound = 0, hash;
 
   do {
-    let hash;
+    const md5 = createHash('md5');
+    md5.update(input + i++, 'ascii');
+    hash = md5.digest('hex');
 
-    do {
-      const md5 = crypto.createHash('md5');
-      md5.update(input + i++);
-      hash = md5.digest('hex');
-    } while (!hash.startsWith(HASH_PREFIX));
+    if (!hash.startsWith(HASH_PREFIX)) {
+      continue;
+    }
 
     // Found a hash starting with `00000`
     const char5 = hash[5];
@@ -46,9 +45,9 @@ module.exports = input => {
     if (pos < PASSWORD_LENGTH && !password2[pos]) {
       // Found a character for part 2
       password2[pos] = hash[6];
-      password2Length++;
+      password2CharsFound++;
     }
-  } while (password2Length !== PASSWORD_LENGTH);
+  } while (password2CharsFound !== PASSWORD_LENGTH);
 
   return [ password1, password2.join('') ];
 };
